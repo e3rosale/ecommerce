5274f36f2134178157fe11bb16088697e70a45a6
e3rosale <eduardo.d.rosales1@gmail.com>
2024-03-05 00:03:29 -0800

    Removing the console log statement in this file

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -14,7 +14,7 @@ export async function Header() {
   try {
     header = await fetchHeader()
   } catch (error) {
-    console.error(error)
+    console.log(error)
   }
 
   return (
@@ -22,26 +22,4 @@ export async function Header() {
       <HeaderComponent header={header} />
     </>
   )
-
-  // return (
-  //   <>
-  //     <header className={classes.header}>
-  //       <Gutter className={classes.wrap}>
-  //         <Link href="/">
-  //           {/* Cannot use the `<picture>` element here with `srcSet`
-  //             This is because the theme is able to be overridden by the user
-  //             And so `@media (prefers-color-scheme: dark)` will not work
-  //             Instead, we just use CSS to invert the color via `filter: invert(1)` based on `[data-theme="dark"]`
-  //           */}
-  //           <img
-  //             className={classes.logo}
-  //             alt="Payload Logo"
-  //             src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/payload/src/admin/assets/images/payload-logo-light.svg"
-  //           />
-  //         </Link>
-  //         <HeaderNav header={header} />
-  //       </Gutter>
-  //     </header>
-  //   </>
-  // )
 }
