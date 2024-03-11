d2c63ddfca74923206b4249e096e0ae7f31560c4
e3rosale <eduardo.d.rosales1@gmail.com>
2024-03-10 23:32:19 -0700

    Sorted import statements in Footer index.tsx

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import { Footer } from '../../../payload/payload-types'
 import { fetchFooter } from '../../_api/fetchGlobals'
-
 import FooterComponent from './FooterComponent'
 
 export async function Footer() {
@@ -11,7 +10,7 @@ export async function Footer() {
   try {
     footer = await fetchFooter()
   } catch (error) {
-    console.log(error)
+    //console.log(error)
   }
 
   const navItems = footer?.navItems || []
